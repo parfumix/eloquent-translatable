98554ca1743dfcb2671459d0d9e40a7f9b3acf0d
Radu <radu.bordei89@gmail.com>
2015-08-25 11:39:33 +0300

    adding required functions.

--- src/Translatable.php
+++ src/Translatable.php
@@ -4,4 +4,11 @@ namespace Eloquent\Translatable;
 
 interface Translatable {
 
+    /**
+     * Translate eloquent .
+     *
+     * @param null $locale
+     * @return mixed
+     */
+    public function translate($locale = null);
 }
